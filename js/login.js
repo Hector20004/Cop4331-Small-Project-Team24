8cc26db5e39c62ab7a7b3c4d365ec9d8d06562a6
Hector20004 <hg482414@gmail.com>
2026-02-03 19:51:01 -0500

    changed payload

--- js/login.js
+++ js/login.js
@@ -39,7 +39,7 @@ function onSignup(){
     let password = document.getElementById("signupPassword").value;
     let hash = md5(password);
 
-    let json = {firstName:firstName,lastName:lastName,username:username,password:hash};
+    let json = {FirstName:firstName,LastName:lastName,Login:username,Password:hash};
     let jsonPayload = JSON.stringify(json);
 
     let xhr = new XMLHttpRequest();
